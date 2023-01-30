84b508a49767c6bb4745586a1fc4b86b1dff1c92
wangxuchu <wangxuchu@kurogame.com>
2023-01-30 19:40:14 +0800

    更新协程功能

---
+++ Assets/_Scripts/Input.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Myd.Platform.Demo
+{
+    public struct VisualButton
+    {
+        private KeyCode key;
+
+        public VisualButton(KeyCode key)
+        {
+            this.key = key;
+        }
+
+        public bool Pressed()
+        {
+            return UnityEngine.Input.GetKeyDown(key);
+        }
+
+        public bool Checked()
+        {
+            return UnityEngine.Input.GetKey(key);
+        }
+    }
+    public static class Input
+    {
+        public static VisualButton Jump = new VisualButton(KeyCode.Space);
+        public static VisualButton Dash = new VisualButton(KeyCode.K);
+    }
+
+
+}
