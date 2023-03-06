707029019ff90892a37dbd9d821be680fe56f80a
david <3357346@qq.com>
2023-03-06 19:28:58 +0800

    更新后处理

--- Assets/_Arts/Materials/Shader/RippleEffect.shader
+++ Assets/_Arts/Materials/Shader/RippleEffect.shader
@@ -31,7 +31,6 @@ Shader "Unlit/RippleEffect"
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
             };
 
---
+++ Assets/_Scripts/PostProcess/RippleEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._Scripts.PostProcess
+{
+    
+    public class RippleEffect : MonoBehaviour
+    {
+        public Material material;
+
+        private void Awake()
+        {
+        }
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, material);
+        }
+
+
+    }
+}
